10f41963fcf75346ab6b79b0e2b498d8fe61d6bb
Naga Bhargavi Marudhuri <bhargavimarudhuri@gmail.com>
2020-09-25 14:59:54 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,16 +1,2 @@
 
-
-while numbers > 3 do
-
-numbers = [1,2,3]
-
-square_array(numbers)
-# => [1,4,9])
-
-new_numbers = [9,10,16,25]
-
-square_array(new_numbers)
-# => [81,100,256,625]
-
-end
-
+while 
